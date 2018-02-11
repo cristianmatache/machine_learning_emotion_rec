dd9111bf7739fdd5f07146c5db4b0dc18a0265e5
Adi Catana <adrian.catana15@imperial.ac.uk>
2018-02-11 11:00:04 +0000

    Started mocking data

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import random as rand
 import cross_validation
+import numpy as np
 from node import TreeNode
 import utilities as util
 import decision_tree_maker as dtree
 
 # Macros
-AU_INDICES = list(range(1, 46))
 emotion = {'anger': 1, 'disgust': 2, 'fear': 3, 'happiness': 4, 'sadness': 5, 'surprise': 6}
 
 
@@ -53,60 +53,93 @@ def compute_confusion_matrix(segments, df_labels, df_data, N):
     def slice_segments(from_index, to_index):
         return df_data[from_index : to_index + 1], df_labels[from_index : to_index + 1]
 
-    res = pd.DataFrame(0, index=[1, 2, 3, 4, 5, 6], columns=[1, 2, 3, 4, 5, 6])
+    res = pd.DataFrame(0, index=[0, 1, 2, 3, 4, 5], columns=[0, 1, 2, 3, 4, 5])
+
+    # print(res)
 
     for test_seg in segments:
         T = []
         test_df_data, test_df_targets, train_df_data, train_df_targets = util.get_train_test_segs(test_seg, N, slice_segments)
 
-        for e in ["happiness"]: #["anger", "disgust", "fear", "happiness", "sadness", "surprise"]:
+        # CORECT
+        print("Test df data")
+        print(test_df_data)
+        print("--------------")
+        print("Test df targets")
+        print(test_df_targets)
+        print("==============")
+        print("Train df data")
+        print(train_df_data)
+        print("--------------")
+        print("Train df targets")
+        print(train_df_targets)
+        print("==============")
+
+        for e in ["anger", "disgust", "fear", "happiness", "sadness", "surprise"]:
             print("Building decision tree for emotion: ", e)
             train_binary_targets = util.filter_for_emotion(train_df_targets, emotion[e])
+            print("Binary train targets", train_binary_targets)
+            print()
 
-            print("******************** TRAIN TARGETS INAINTE SI DUPA FILTRARE ************")
-            pd_join = pd.concat([train_df_targets, train_binary_targets], axis=1)
-            print(pd_join)
+            # print("******************** TRAIN TARGETS INAINTE SI DUPA FILTRARE ************")
+    #         pd_join = pd.concat([train_df_targets, train_binary_targets], axis=1)
+    #         print(pd_join)
 
             root = dtree.decision_tree(train_df_data, set(AU_INDICES), train_binary_targets)
             print("Decision tree built.")
             T.append(root)
 
-        predictions = test_trees(T, test_df_data, test_df_targets)
-        confusion_matrix = compare_pred_expect(predictions, test_df_targets)
-        # print("Confusion matrix", confusion_matrix)
+            # predictions = test_trees(T, test_df_data, test_df_targets)
+     # confusion_matrix = compare_pred_expect(predictions, test_df_targets)
+    #     # print("Confusion matrix", confusion_matrix)
 
-        # print("Confusion matrix", confusion_matrix.div(confusion_matrix.sum(axis=1), axis=0))
-        res = res.add(confusion_matrix)
+    #     # print("Confusion matrix", confusion_matrix.div(confusion_matrix.sum(axis=1), axis=0))
+    #     res = res.add(confusion_matrix)
 
 
-    return res.div(res.sum(axis=1), axis=0)
+    res = res.div(res.sum(axis=1), axis=0)
+    return res
 
+AU_INDICES = list(range(1, 10))
 
 # Testing
 def main():
-    labels, data = util.load_raw_data()
-    df_labels, df_data = util.to_dataframe(labels, data)
+
+    MOCK_SIZE = 10
+
+    df_data_MOCK = pd.DataFrame(np.random.randint(low=0, high=2, size=(MOCK_SIZE, MOCK_SIZE)))
+
+    df_labels_MOCK = pd.DataFrame(np.random.randint(low=1, high=7, size=(MOCK_SIZE, 1)))
+
+    print(df_data_MOCK)
+    print("============")
+    print(df_labels_MOCK)
+
+#    labels, data = util.load_raw_data()
+
+#    A = np.array(labels)
+
+#    labels = [row[0] for row in A]
+
+#    df_labels, df_data = util.to_dataframe(labels, data)
+
+    N_MOCK = MOCK_SIZE
+
+#    print("Df_labels", df_labels)
+#    print("Df_Data", df_data)
 
     # Number of examples
-    N = df_labels.shape[0]
-    segments = util.preprocess_for_cross_validation(N)
+#    N = df_labels.shape[0]
+    segments = util.preprocess_for_cross_validation(N_MOCK)
     print("----------------------------------- LOADING COMPLETED ----------------------------------- \n")
 
-    # cross_validation.cross_validation_error(df_labels, N, df_data, segments)
-    print(compute_confusion_matrix(segments, df_labels, df_data, N))
+    compute_confusion_matrix(segments, df_labels_MOCK, df_data_MOCK, N_MOCK)
 
-#    print(choose_prediction([0, 0, 1, 0, 0, 0]))
-#    bins = util.filter_for_emotion(df_labels, emotion['surprise'])
+    # for e in emotion.keys():
+    #     binary_targets = util.filter_for_emotion(df_labels, emotion[e])
+    #     root = dtree.decision_tree(df_data, set(AU_INDICES), binary_targets)
+    #     for i in range(0, len(df_labels)):
+    #         TreeNode.dfs2(root, df_data.loc[i], binary_targets.loc[i].at[0])
 
-    for e in emotion.keys():
-        binary_targets = util.filter_for_emotion(df_labels, emotion[e])
-        root = dtree.decision_tree(df_data, set(AU_INDICES), binary_targets)
-        for i in range(0, len(df_labels)):
-            TreeNode.dfs2(root, df_data.loc[i], binary_targets.loc[i].at[0])
-
-
-    # for index, row in df_labels.iterrows():
-    #     print(df_labels.iloc[index])
-    #     print("==============")
 
 if __name__ == "__main__": main()
